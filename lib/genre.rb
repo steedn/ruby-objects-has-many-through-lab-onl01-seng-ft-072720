bbd5275b0f0ee9abd23caa0e335a6cc305e5947c
Nathan Steed <steedn@outlook.com>
2020-08-17 02:34:55 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,26 @@
 class Genre
-  attr_accessor :genre
-  def initialize(genre)
-    @genre = genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
   end
 end
