8ca6617a456d05692ac6619827c060ec585389b5
Nathan Steed <steedn@outlook.com>
2020-08-17 02:58:45 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,15 +1,32 @@
+# class Appointment
+#     @@all = []
+#     attr_accessor :appointment, :doctor, :patient
+#     def initialize(appointment, patient, doctor)
+#       @doctor = doctor
+#       @patient = patient
+#       @appointment = appointment
+#       @@all << self
+#     end
+#     def self.all
+#       @@all
+#     end
+#     def patient
+#     end
+#   end
+
 class Appointment
-    @@all = []
-    attr_accessor :appointment, :doctor, :patient
-    def initialize(appointment, patient, doctor)
-      @doctor = doctor
-      @patient = patient
-      @appointment = appointment
-      @@all << self
-    end
-    def self.all
-      @@all
-    end
-    def patient
-    end
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+end
