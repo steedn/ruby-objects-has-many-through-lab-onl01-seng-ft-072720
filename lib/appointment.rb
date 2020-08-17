4d88e56755162483b944bd26a70efd8305a33599
Nathan Steed <steedn@outlook.com>
2020-08-17 02:59:21 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,19 +1,3 @@
-# class Appointment
-#     @@all = []
-#     attr_accessor :appointment, :doctor, :patient
-#     def initialize(appointment, patient, doctor)
-#       @doctor = doctor
-#       @patient = patient
-#       @appointment = appointment
-#       @@all << self
-#     end
-#     def self.all
-#       @@all
-#     end
-#     def patient
-#     end
-#   end
-
 class Appointment
   attr_accessor :date, :doctor, :patient
 
